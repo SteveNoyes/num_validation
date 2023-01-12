686f0382d1afe5ddcdd2d5d4c092eceb7751bc47
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-12 18:49:32 -0500

    regex pattern

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Regex pattern
+
+let bracketCheck = /[(]\d{3}[)]/;
+let checkBeginning = /^([(]\d{3}[)])/;
+let otherCharsCheck = /^[-]|[^\s-\d)(]/;
+
+// Document Object Model
+
 const input = document.getElementById('input');
 const buttons = document.querySelectorAll('.num-btn');
 const resultsDiv = document.getElementById('results');
