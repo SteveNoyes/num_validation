062244efe9da2e263bfe91a117f3c785acaa5fe3
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-12 18:24:28 -0500

    js start

---
+++ app.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('.num-btn');
+const resultsDiv = document.getElementById('results');
+const phoneBtn = document.getElementById('phone-button');
