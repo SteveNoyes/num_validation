0971cd981df226c3a654534633a3ffdf16245156
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-12 18:56:38 -0500

    for each button

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 // Regex pattern
 
-let bracketCheck = /[(]\d{3}[)]/;
-let checkBeginning = /^([(]\d{3}[)])/;
-let otherCharsCheck = /^[-]|[^\s-\d)(]/;
-let space = /[\s-]/g;
-let countryCodeCheck = /^1/;
-let simple = str.replace(space, '');
-if (otherCharsCheck.test(str)) {
-  return false;
-} else {
-  if (simple.length === 13
-      && countryCodeCheck.test(simple)
-      && bracketCheck.test(simple)) {
-      return true;
-  } else if (simple.length === 12
-      && checkBeginning.test(simple)) {
-      return true;
-  } else if (simple.length === 11
-      && countryCodeCheck.test(simple)) {
-      return true;
-  } else if (simple.length === 10) {
-      return true;
-  }
-} return false;
+function mobileCheck(str) {
+  let bracketCheck = /[(]\d{3}[)]/;
+  let checkBeginning = /^([(]\d{3}[)])/;
+  let otherCharsCheck = /^[-]|[^\s-\d)(]/;
+  let space = /[\s-]/g;
+  let countryCodeCheck = /^1/;
+  let simple = str.replace(space, '');
+  if (otherCharsCheck.test(str)) {
+      return false;
+  } else {
+      if (simple.length === 13
+          && countryCodeCheck.test(simple)
+          && bracketCheck.test(simple)) {
+          return true;
+      } else if (simple.length === 12
+          && checkBeginning.test(simple)) {
+          return true;
+      } else if (simple.length === 11
+          && countryCodeCheck.test(simple)) {
+          return true;
+      } else if (simple.length === 10) {
+          return true;
+      }
+  } return false;
+}
 
 // Document Object Model
 
@@ -30,3 +32,17 @@ const input = document.getElementById('input');
 const buttons = document.querySelectorAll('.num-btn');
 const resultsDiv = document.getElementById('results');
 const phoneBtn = document.getElementById('phone-button');
+
+// Button
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    let number;
+    if (e.target.tagName == 'SPAN' || e.target.tagName == 'P') {
+      number = e.target.parentElement.attributes.number.value;
+    } else {
+      number = e.target.attributes.number.value;
+    }
+    input.value += number;
+  });
+});
